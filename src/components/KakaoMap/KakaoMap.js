598529a33cc04a8b80f1a2b4b7c8256314609c70
unknown <jaywapp16@gmail.com>
2023-03-01 14:34:47 +0900

    add fitering data and marking data

--- src/components/KakaoMap/KakaoMap.js
+++ src/components/KakaoMap/KakaoMap.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import Datas from '../../datas/data'
 
 const { kakao } = window;
 
@@ -39,3 +40,128 @@ export function ResizeKakaoMap( w, h ){
         kakaoMap.relayout();
     }
 }
+
+export function Pick(x, y, desc){
+    var coords = new kakao.maps.LatLng(y, x);
+
+    // 결과값으로 받은 위치를 마커로 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: kakaoMap,
+        position: coords
+    });
+
+    // 인포윈도우로 장소에 대한 설명을 표시합니다
+    var infowindow = new kakao.maps.InfoWindow({
+        content:  '<div style="width:150px;text-align:center;padding:6px 0;">' + desc + '</div>'
+    });
+    
+    infowindow.open(kakaoMap, marker);
+}
+
+export function DisplayMarker(lat, lon){
+
+    var locPosition = new kakao.maps.LatLng(lat, lon)
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({  
+        map: kakaoMap, 
+        position: locPosition
+    }); 
+    
+    var iwContent = '<div style="padding:5px;color:red;">You are Here!</div>', // 인포윈도우에 표시할 내용
+        iwRemoveable = true;
+
+    // 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({
+        content : iwContent,
+        removable : iwRemoveable
+    });
+    
+    // 인포윈도우를 마커위에 표시합니다 
+    infowindow.open(kakaoMap, marker);
+    
+    // 지도 중심좌표를 접속위치로 변경합니다
+    kakaoMap.setCenter(locPosition);  
+}
+
+export function PickAround(distance){
+
+    if(navigator.geolocation){
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+            
+            var lat = position.coords.latitude;
+            var lon = position.coords.longitude;
+
+            DisplayMarker(lat, lon);
+
+            var aroundDatas = GetDatas(lat, lon, distance);
+            var points = aroundDatas.map(data => {
+                return new kakao.maps.LatLng(data.y, data.x);
+            })
+
+            points.push(new kakao.maps.LatLng(lat, lon));
+
+            var bounds = new kakao.maps.LatLngBounds();    
+
+            var i, marker;
+            for (i = 0; i < points.length; i++) {
+                // 배열의 좌표들이 잘 보이게 마커를 지도에 추가합니다
+                marker = new kakao.maps.Marker({ position : points[i] });
+                marker.setMap(kakaoMap);
+                
+                // LatLngBounds 객체에 좌표를 추가합니다
+                bounds.extend(points[i]);
+            }
+
+            aroundDatas.forEach(data => Pick(data.x, data.y, data.name));
+
+            kakaoMap.setBounds(bounds);
+        });
+
+        console.log('fail to get current position');
+    }
+    else{
+
+        console.log('Can not use geolocation on HTML5');
+    }
+}
+
+function GetDatas(lat, lon, distance){
+    var arr = new Array();
+
+    Datas.forEach(data=>{
+
+        var d = GetDistance(lat, lon, data.y, data.x);
+        console.log('name : ' + data.name + '/' + 'distance : ' + d);
+
+        if(d <= distance){
+            console.log('▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲');
+            arr.push(data);
+        }
+    })
+
+    console.log(arr.length);
+
+    return arr;
+}
+
+function GetDistance(lat1, lon1, lat2, lon2){
+      
+    var R = 6371; // km
+    var dLat = toRad(lat2-lat1);
+    var dLon = toRad(lon2-lon1);
+    var lat1 = toRad(lat1);
+    var lat2 = toRad(lat2);
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c;
+    return d;
+}
+
+function toRad(Value) 
+{
+    return Value * Math.PI / 180;
+}
+
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,28 +1,50 @@
 import Datas from '../../datas/data'
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { PickAround, kakaoMap, DisplayMarker, Pick, GetCurrentPosition } from '../KakaoMap/KakaoMap';
 
 const mainColor = '#2a2b34';
 const subColor = '#3d3d4c';
 
 const MenuDiv = styled.div`
-  display: grid;
-  grid-template-columns: 300px 1fr;
-  grid-row: 1;
-  background-color: ${mainColor};
+    display: grid;
+    grid-template-columns: 300px 1fr 1fr 1fr 1fr 1fr;
+    grid-row: 1;
+    background-color: ${mainColor};
 `;
 
 const LogoDiv = styled.h1`
-  grid-row: 1;
-  color: white;
+    grid-row: 1;
+    color: white;
+`;
+
+const Button = styled.button`
+    grid-column: ${(props) => props.index + 1};
+    background-color: ${subColor};
+    color: white;
+    font-size: 30;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    margin-left: 10px;
+    margin-right: 10px;
 `;
 
 function Menu(){
   return (
     <MenuDiv>
       <LogoDiv>토방으로뛰어</LogoDiv>
+      <Button index={1} onClick={() => Collect(1)}>반경 1KM</Button>
+      <Button index={2} onClick={() => Collect(3)}>반경 3KM</Button>
+      <Button index={3} onClick={() => Collect(5)}>반경 5KM</Button>
+      <Button index={4} onClick={() => Collect(7)}>반경 7KM</Button>
+      <Button index={5} onClick={() => Collect(10)}>반경 10KM</Button>
     </MenuDiv>
   );
+}
+
+function Collect(distance){
+
+    PickAround(distance);
 }
 
 export default Menu;
